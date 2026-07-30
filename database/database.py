bb09e6f6f9e6dc8d2538a9d59c634409a38bc67f
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-07-31 00:41:55 +0530

    Add database connection setup and remove main script

---
+++ database/database.py
@@ -0,0 +1,26 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from dotenv import load_dotenv
+from os import getenv
+
+load_dotenv()
+
+DATABASE_URL = getenv("DATABASE_URL")
+
+if DATABASE_URL is None:
+    raise ValueError("DATABASE_URL is not set.")
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo = False
+)
+
+SessionLocal = async_sessionmaker(
+    bind = engine,
+    expire_on_commit = False,
+    autoflush = False
+)
+
+class Base(DeclarativeBase):
+    pass
+
