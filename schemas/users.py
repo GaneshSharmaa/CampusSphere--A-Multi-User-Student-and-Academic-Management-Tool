3b373165d5f4753d7e1666a8fbd47c941a23aeeb
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-03 17:56:31 +0530

    Add user schemas for creation, response, and login validation

---
+++ schemas/users.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+from pydantic import BaseModel, ConfigDict, EmailStr, PastDate
+
+# schema for request validation - user creating / registering
+class UserCreate(BaseModel):
+    first_name: str
+    last_name: str
+    dob: PastDate
+    phone: str
+    email: EmailStr
+    password: str
+
+# schema for response model - user response
+class UserResponse(BaseModel):
+    first_name: str
+    last_name: str
+    phone: str
+    email: EmailStr
+
+# schema for request validation - user login
+class UserLogin(BaseModel):
+    email: EmailStr
+    password: str
+
