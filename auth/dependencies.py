d5a248cede4d0793226d9f12f75efea36564dff9
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-05 20:20:28 +0530

    Add admin authorization function to restrict access

--- auth/dependencies.py
+++ auth/dependencies.py
@@ -37,3 +37,14 @@ async def get_current_user(
         )
 
     return user
+
+# function to authorize only admin user
+async def require_admin(current_user: Annotated[User, Depends(get_current_user)]):
+    if current_user.role.role_name != "Admin":
+        raise HTTPException(
+            status_code = status.HTTP_403_FORBIDDEN,
+            detail = "Not authorized."
+        )
+
+    return current_user
+
