7915953bdfec7da2aeb80d0cd4b4cd68c5ca7958
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-05 20:31:04 +0530

    Add delete user endpoint with admin access control

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from schemas.users import UserCreate, UserResponse, UserLogin
 from schemas.token import Token
 from auth.hashing import hash_password, verify_password
 from auth.jwt import create_access_token
-from auth.dependencies import get_current_user
+from auth.dependencies import get_current_user, require_admin
 
 # async database creation
 @asynccontextmanager
@@ -95,6 +95,22 @@ async def login(
         "token_type": "bearer"
     }
 
+# --------- PROFILE ROUTE - SHOWS WHICH USER IS LOGGED IN ---------
 @app.get("/me")
-async def me(current_user = Depends(get_current_user)):
+async def me(current_user: Annotated[User, Depends(get_current_user)]):
     return current_user
+
+# -------- DELETE ENDPOINT - ONLY ADMINS CAN DELETE THE USERS --------
+@app.delete("/delete/user/{user_id}", status_code = status.HTTP_204_NO_CONTENT)
+async def delete_user(user_id: int, admin_access: Annotated[User, Depends(require_admin)], db: Annotated[AsyncSession, Depends(get_db)]):
+    user = await db.scalar(select(User).where(User.id == user_id))
+
+    if user is None:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "User not found"
+        )
+
+    await db.delete(user)
+    await db.commit()
+
