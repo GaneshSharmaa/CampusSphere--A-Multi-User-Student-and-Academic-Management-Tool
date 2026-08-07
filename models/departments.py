a6fbab02262906587cbdada1e8e8c1996eefed42
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-08 00:21:17 +0530

    Add Department model to manage department information and relationships

---
+++ models/departments.py
@@ -0,0 +1,42 @@
+# importing local modules
+from database.database import Base
+
+# importing the required modules
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, Sequence, DateTime, func
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models.students import Student
+
+class Department(Base):
+    __tablename__ = "departments"
+
+    dept_code: Mapped[str] = mapped_column(
+        String(10),
+        primary_key = True,
+        index = True
+    )
+    dept_name: Mapped[str] = mapped_column(
+        String(50),
+        unique = True,
+        index = True,
+        nullable = False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.now(),
+        nullable = False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.now(),
+        onupdate = func.now(),
+        nullable = False
+    )
+
+    students: Mapped[list["Student"]] = relationship(
+        back_populates = "department"
+    )
+
