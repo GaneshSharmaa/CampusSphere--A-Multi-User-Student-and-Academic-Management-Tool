7290cb5069e8bf0071432f212961af29978b6ad0
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-08 19:51:48 +0530

    Refactor user schemas by removing unused fields and updating response model

--- schemas/users.py
+++ schemas/users.py
@@ -3,25 +3,15 @@ from pydantic import BaseModel, ConfigDict, EmailStr, PastDate
 
 # schema for request validation - user creating / registering
 class UserCreate(BaseModel):
-    first_name: str
-    last_name: str
-    dob: PastDate
-    phone: str
     email: EmailStr
+    phone: str
     password: str
-
-# schema for updating user
-class UserUpdate(BaseModel):
-    first_name: str | None = None
-    last_name: str | None = None
-    dob: PastDate | None = None
 
 # schema for response model - user response
 class UserResponse(BaseModel):
-    first_name: str
-    last_name: str
-    phone: str
+    id: int
     email: EmailStr
+    phone: str
 
     model_config = ConfigDict(from_attributes = True)
 
@@ -32,9 +22,6 @@ class UserLogin(BaseModel):
 
 # schema for query parameters
 class UserQueryParams(BaseModel):
-    first_name: str | None = None
-    last_name: str | None = None
     email: str | None = None
     phone: str | None = None
-    dob: PastDate | None = None
 
