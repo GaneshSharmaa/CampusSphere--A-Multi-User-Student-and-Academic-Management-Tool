f2dfc54cbf501f354baee0a5c0d79206a546daf2
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-08 19:25:19 +0530

    Add Faculty model with relationships and updated_at field

---
+++ models/faculty.py
@@ -0,0 +1,91 @@
+# importing local modules
+from database.database import Base
+
+# importing required modules
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, Text, Integer, DateTime, Date, func
+from sqlalchemy import Enum as SQLEnum
+from datetime import datetime, date
+from enum import Enum
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models.roles import Role
+    from models.departments import Department
+    from models.users import User
+
+class SexEnum(Enum):
+    MALE = "Male"
+    FEMALE = "Female"
+    OTHER = "Other"
+
+class Faculty(Base):
+    __tablename__ = "faculty"
+
+    emp_id: Mapped[int] = mapped_column(
+        primary_key = True,
+        index = True
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+        unique = True,
+        nullable = False,
+        index = True 
+    )
+    first_name: Mapped[str] = mapped_column(
+        String(25),
+        nullable = False
+    )
+    last_name: Mapped[str] = mapped_column(
+        String(25),
+        nullable = False
+    )
+    dob: Mapped[date] = mapped_column(
+        Date,
+        nullable = False
+    )
+    sex: Mapped[SexEnum] = mapped_column(
+        SQLEnum(SexEnum),
+        nullable = False
+    )
+    address: Mapped[str] = mapped_column(
+        Text,
+        nullable = False
+    )
+    date_of_joining: Mapped[date] = mapped_column(
+        Date,
+        nullable = False
+    )
+    department: Mapped[str] = mapped_column(
+        ForeignKey("departments.dept_name"),
+        nullable = False
+    )
+    role: Mapped[str] = mapped_column(
+        ForeignKey("roles.role_name"),
+        nullable = False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.timezone("Asia/Kolkata", func.now()),
+        nullable = False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.timezone("Asia/Kolkata", func.now()),
+        server_onupdate = func.timezone("Asia/Kolkata", func.now()),
+        nullable = False
+    )
+
+    # relationships
+    role: Mapped["Role"] = relationship(
+        back_populates = "faculty"
+    )
+
+    dept: Mapped["Department"] = relationship(
+        back_populates = "faculties"
+    )
+
+    user: Mapped["User"] = relationship(
+        back_populates = "faculty"
+    )
+
