98ae12041678c40d0ebfa73ce85f55240e86b13e
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-08 19:25:34 +0530

    Enhance Student model with additional fields and relationships, including end_batch_year, created_at, and updated_at

--- models/students.py
+++ models/students.py
@@ -1,9 +1,11 @@
 # importing the local module
 from database.database import Base
+from models.faculty import SexEnum
 
 # importing the required modules
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from sqlalchemy import ForeignKey, String, Sequence, Date, DateTime, func, Identity
+from sqlalchemy import ForeignKey, String, Text, Integer, Sequence, Date, DateTime, func, Identity, CheckConstraint
+from sqlalchemy import Enum as SQLEnum
 from datetime import datetime, date
 from typing import TYPE_CHECKING
 
@@ -30,20 +32,62 @@ class Student(Base):
         nullable = False,
         index = True
     )
-    dept_code: Mapped[str] = mapped_column(
-        ForeignKey("departments.dept_code"),
+    first_name: Mapped[str] = mapped_column(
+        String(25),
         nullable = False
     )
-    admission_date: Mapped[date] = mapped_column(
+    last_name: Mapped[str] = mapped_column(
+        String(25),
+        nullable = False
+    )
+    dob: Mapped[date] = mapped_column(
         Date,
         nullable = False
     )
+    sex: Mapped[SexEnum] = mapped_column(
+        SQLEnum(SexEnum),
+        nullable = False
+    )
+    address: Mapped[str] = mapped_column(
+        Text,
+        nullable = False
+    )
+    date_of_admission: Mapped[date] = mapped_column(
+        Date,
+        nullable = False
+    )
+    department: Mapped[str] = mapped_column(
+        ForeignKey("departments.dept_name"),
+        nullable = False
+    )
+    start_batch_year: Mapped[int] = mapped_column(
+        Integer,
+        CheckConstraint("start_batch_year >= 1970 AND start_batch_year <= 2100"),
+        nullable = False
+    )
+    end_batch_year: Mapped[int] = mapped_column(
+        Integer,
+        CheckConstraint("end_batch_year >= 1970 AND end_batch_year <= 2100"),
+        nullable = False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.timezone("Asia/Kolkata", func.now()),
+        nullable = False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.timezone("Asia/Kolkata", func.now()),
+        server_onupdate = func.timezone("Asia/Kolkata", func.now()),
+        nullable = False
+    )
 
+    # relationships
     user: Mapped["User"] = relationship(
         back_populates = "student"
     )
 
-    department: Mapped["Department"] = relationship(
+    dept: Mapped["Department"] = relationship(
         back_populates = "students"
     )
 
