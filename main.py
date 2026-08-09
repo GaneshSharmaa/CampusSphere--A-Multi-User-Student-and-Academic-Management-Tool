7ab16ac13744920c14b04e4069479a18afc805d8
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-09 19:42:32 +0530

    Add role and faculty creation endpoints with validation checks

--- main.py
+++ main.py
@@ -18,14 +18,16 @@ from models.students import Student
 from models.users import User
 
 # importing local schemas
-from schemas.users import UserCreate, UserResponse, UserLogin, UserQueryParams
+from schemas.users import UserCreate, UserResponse, UserLogin
 from schemas.token import Token
 from schemas.departments import CreateDepartment, ResponseDepartment
+from schemas.faculty import FacultyCreate, FacultyResponse
+from schemas.roles import RoleCreate, RoleResponse
 
 # importing authentication and authorization local modules
 from auth.hashing import hash_password, verify_password
 from auth.jwt import create_access_token
-from auth.dependencies import get_current_user, admin_teacher_access
+from auth.dependencies import get_current_user, admin_access, professor_access, hod_access, principal_access
 
 # async database creation
 @asynccontextmanager
@@ -36,7 +38,7 @@ async def lifespan(app: FastAPI):
     await engine.dispose()
 
 # initializing the app
-app = FastAPI(lifespan = lifespan)
+app = FastAPI(lifespan = lifespan, title = "CampusSphere: A Multi-User Student and Academic Management Tool")
 
 # -------- HOME ROUTE --------
 @app.get("/")
@@ -112,4 +114,70 @@ async def login(
         "access_token": access_token,
         "token_type": "bearer"
     }
+
+# --------- CREATING ROLE ROUTE ---------
+@app.post("/create-role", response_model = RoleResponse)
+async def create_role(
+    role: RoleCreate,
+    access: Annotated[User, Depends(admin_access)],
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+    role_chk = await db.scalar(
+        select(Role).where(Role.role_name == role.role_name)
+    )
+
+    if role_chk is not None:
+        raise HTTPException(
+            status_code = status.HTTP_409_CONFLICT,
+            detail = "Role already exists."
+        )
+
+    db.add(role)
+    await db.commit()
+    await db.refresh(role)
+
+# CREATING FACULTY ROUTE
+@app.post("/create-faculty", response_model = FacultyResponse)
+async def create_faculty(
+    faculty: FacultyCreate,
+    access: Annotated[User, Depends(admin_access)],
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+    faculty_exists = await db.scalar(
+        select(Faculty).where(Faculty.user_id == faculty.user_id)
+    )
+
+    if faculty_exists is not None:
+        raise HTTPException(
+            status_code = status.HTTP_409_CONFLICT,
+            detail = "Faculty already exists."
+        )
+
+    user_exists = await db.scalar(
+        select(User).where(User.id == faculty.user_id)
+    )
+
+    if user_exists is None:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "User not found."
+        )
+
+    faculty_detail = Faculty(
+        user_id = faculty.user_id,
+        first_name = faculty.first_name,
+        last_name = faculty.last_name,
+        dob = faculty.dob,
+        sex = faculty.sex,
+        address = faculty.address,
+        date_of_joining = faculty.date_of_joining,
+        dept_code = faculty.dept_code,
+        role_id = faculty.role_id,
+    )
+
+    db.add(faculty_detail)
+    await db.commit()
+    await db.refresh(faculty_detail)
+
+    return faculty_detail
 
