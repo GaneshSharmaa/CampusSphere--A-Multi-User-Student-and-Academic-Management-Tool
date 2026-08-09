1f03284d2f741cbb48a5607bf126994477bbbc20
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-09 22:38:01 +0530

    Refactor main.py and add user authentication routes in users.py

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
-# importing required modules
+# Importing required modules
 from fastapi import FastAPI, HTTPException, status, Depends, Query
 from typing import Annotated
 from contextlib import asynccontextmanager
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
-from fastapi.security import OAuth2PasswordRequestForm
 
-# importing database related local modules
+# Importing database dependencies
 from database.database import engine
 from database.dependencies import get_db
 
-# importing local database models
-from models.departments import Department
+# Importing database models
 from models.faculty import Faculty
 from models.roles import Role
-from models.students import Student
 from models.users import User
 
-# importing local schemas
-from schemas.users import UserCreate, UserResponse, UserLogin
-from schemas.token import Token
-from schemas.departments import CreateDepartment, ResponseDepartment
+# Importing schemas
 from schemas.faculty import FacultyCreate, FacultyResponse
 from schemas.roles import RoleCreate, RoleResponse
 
-# importing authentication and authorization local modules
-from auth.hashing import hash_password, verify_password
-from auth.jwt import create_access_token
+# Importing authentication and authorization modules
 from auth.dependencies import get_current_user, admin_access, professor_access, hod_access, principal_access
 
-# async database creation
+# Importing routes
+from routes import users
+
+# Async database creation
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # startup
@@ -37,82 +32,24 @@ async def lifespan(app: FastAPI):
     # shutdown
     await engine.dispose()
 
-# initializing the app
-app = FastAPI(lifespan = lifespan, title = "CampusSphere: A Multi-User Student and Academic Management Tool")
+# Initializing the app
+app = FastAPI(
+    title = "CampusSphere: A Multi-User Student and Academic Management Tool",
+    lifespan = lifespan
+)
+
+# Include the `Auth` router
+app.include_router(
+    users.router,
+    prefix = "/users",
+    tags = ["User Management"]
+)
 
 # -------- HOME ROUTE --------
 @app.get("/")
 async def home():
     return {
         "message": "Hello!"
-    }
-
-# ------ REGISTER ROUTE - CREATING NEW USER ------
-@app.post("/register", response_model = UserResponse)
-async def create_new_user(user: UserCreate, db: Annotated[AsyncSession, Depends(get_db)]):
-    email_chk = await db.scalar(
-        select(User).where(user.email == User.email)
-    )
-
-    phone_chk = await db.scalar(
-        select(User).where(User.phone == user.phone)
-    )
-
-    if email_chk is not None:
-        raise HTTPException(
-            status_code = status.HTTP_409_CONFLICT,
-            detail = "User with this email already exists!"
-        )
-    
-    if phone_chk is not None:
-        raise HTTPException(
-            status_code = status.HTTP_409_CONFLICT,
-            detail = "User with this phone already exists!"
-        )
-
-    new_user = User(
-        email = user.email,
-        phone = user.phone,
-        hashed_password = hash_password(user.password)
-    )
-
-    db.add(new_user)
-    await db.commit()
-    await db.refresh(new_user)
-
-    return new_user
-
-# ------- LOGIN ROUTE - AUTHENTICATING USERS -------
-@app.post("/login", response_model = Token)
-async def login(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
-    db: Annotated[AsyncSession, Depends(get_db)]
-):
-    user = await db.scalar(
-        select(User).where(User.email == form_data.username)
-    )
-
-    if user is None:
-        raise HTTPException(
-            status_code = status.HTTP_401_UNAUTHORIZED,
-            detail = "Invalid email or password."
-        )
-
-    if not verify_password(form_data.password, user.hashed_password):
-        raise HTTPException(
-            status_code = status.HTTP_401_UNAUTHORIZED,
-            detail = "Invalid email or password."
-        )
-
-    access_token = create_access_token(
-        {
-            "sub": str(user.id)
-        }
-    )
-
-    return {
-        "access_token": access_token,
-        "token_type": "bearer"
     }
 
 # --------- CREATING ROLE ROUTE ---------
@@ -136,7 +73,7 @@ async def create_role(
     await db.commit()
     await db.refresh(role)
 
-# CREATING FACULTY ROUTE
+# --------- CREATING FACULTY ROUTE ---------
 @app.post("/create-faculty", response_model = FacultyResponse)
 async def create_faculty(
     faculty: FacultyCreate,
---
+++ routes/users.py
@@ -0,0 +1,113 @@
+# Importing the required modules
+from fastapi import HTTPException, status, Depends, APIRouter
+from typing import Annotated
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from fastapi.security import OAuth2PasswordRequestForm
+
+# Importing the database dependencies
+from database.dependencies import get_db
+
+# Importing the database models
+from models.users import User
+from models.faculty import Faculty
+
+# Importing the schemas
+from schemas.users import UserCreate, UserResponse, UserLogin
+from schemas.token import Token
+
+# Importing authentication and authorization modules
+from auth.hashing import hash_password, verify_password
+from auth.jwt import create_access_token
+from auth.dependencies import get_current_user, admin_access, professor_access, hod_access, principal_access
+
+router = APIRouter()
+
+# ------ REGISTER ROUTE - CREATING NEW USER ------
+@router.post("/register", response_model = UserResponse)
+async def create_new_user(
+    user: UserCreate,
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+    # Checking if user with this email exists
+    email_chk = await db.scalar(
+        select(User).where(user.email == User.email)
+    )
+
+    # Checking if user with this phone exists
+    phone_chk = await db.scalar(
+        select(User).where(User.phone == user.phone)
+    )
+
+    # If email already exists raise HTTP exception
+    if email_chk is not None:
+        raise HTTPException(
+            status_code = status.HTTP_409_CONFLICT,
+            detail = "User with this email already exists!"
+        )
+
+    # If phone already exists raise HTTP exception
+    if phone_chk is not None:
+        raise HTTPException(
+            status_code = status.HTTP_409_CONFLICT,
+            detail = "User with this phone already exists!"
+        )
+
+    # Setting the variable to enter in database
+    new_user = User(
+        email = user.email,
+        phone = user.phone,
+        hashed_password = hash_password(user.password)
+    )
+
+    # Adding and commiting the changes
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+
+    return new_user
+
+# ------- LOGIN ROUTE - AUTHENTICATING USERS -------
+@router.post("/login", response_model = Token)
+async def login(
+    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+    # Checking if user with this email exists
+    user = await db.scalar(
+        select(User).where(User.email == form_data.username)
+    )
+
+    # If not exists, then raise HTTP exception
+    if user is None:
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Invalid email or password."
+        )
+
+    # Verifying password
+    if not verify_password(form_data.password, user.hashed_password):
+        raise HTTPException(
+            status_code = status.HTTP_401_UNAUTHORIZED,
+            detail = "Invalid email or password."
+        )
+
+    # Creating access token
+    access_token = create_access_token(
+        {
+            "sub": str(user.id)
+        }
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer"
+    }
+
+# ---------- GET USER ROUTE ----------
+@router.get("/me", response_model = UserResponse)
+async def my_profile(
+    current_user: Annotated[User, Depends(get_current_user)]
+):
+    return current_user
+
