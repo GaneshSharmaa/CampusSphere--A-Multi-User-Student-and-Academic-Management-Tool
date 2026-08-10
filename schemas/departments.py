74b65915c03c80fd720d9cc3a76ec15ba0c7320d
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-11 00:52:57 +0530

    Add UpdateDepartment schema to handle optional department updates

--- schemas/departments.py
+++ schemas/departments.py
@@ -9,3 +9,7 @@ class ResponseDepartment(BaseModel):
     dept_code: str
     dept_name: str
 
+class UpdateDepartment(BaseModel):
+    dept_code: str | None = None
+    dept_name: str | None = None
+
