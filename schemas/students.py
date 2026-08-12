7f9541b47896501227957e86b0614bae917b1b90
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-13 00:35:57 +0530

    Add StudentCreate and StudentResponse schemas for student management

---
+++ schemas/students.py
@@ -0,0 +1,36 @@
+# Importing the required modules
+from pydantic import BaseModel, Field, PastDate, ConfigDict
+from datetime import date, datetime
+
+# Importing the Sex Enum
+from models.faculty import SexEnum
+
+# Schema for creating a student
+class StudentCreate(BaseModel):
+    user_id: int
+    first_name: str
+    last_name: str
+    dob: PastDate
+    sex: SexEnum
+    address: str
+    date_of_admission: date
+    dept_code: str
+    role_id: int
+
+# Schema for response model of a student
+class StudentResponse(BaseModel):
+    id: int
+    user_id: int
+    first_name: str
+    last_name: str
+    dob: PastDate
+    sex: SexEnum
+    address: str
+    date_of_admission: date
+    dept_code: str
+    role_id: int
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = ConfigDict(from_attributes = True)
+
