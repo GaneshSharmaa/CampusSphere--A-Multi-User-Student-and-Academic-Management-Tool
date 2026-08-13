e412c85f45aa9d4e2651e38c4aea335270d627df
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-13 22:25:36 +0530

    Implement faculty search, creation, update, and deletion routes with validation

--- routes/faculty.py
+++ routes/faculty.py
@@ -21,51 +21,6 @@ from auth.dependencies import get_current_user, admin_access, professor_access, 
 
 router = APIRouter()
 
-# --------- CREATING FACULTY ROUTE ---------
-@router.post("/create", response_model = FacultyResponse)
-async def create_faculty(
-    faculty: FacultyCreate,
-    access: Annotated[User, Depends(admin_access)],
-    db: Annotated[AsyncSession, Depends(get_db)]
-):
-    user_exists = await db.scalar(
-        select(User).where(User.id == faculty.user_id)
-    )
-
-    if user_exists is None:
-        raise HTTPException(
-            status_code = status.HTTP_404_NOT_FOUND,
-            detail = "User not found."
-        )
-
-    faculty_exists = await db.scalar(
-        select(Faculty).where(Faculty.user_id == faculty.user_id)
-    )
-
-    if faculty_exists is not None:
-        raise HTTPException(
-            status_code = status.HTTP_409_CONFLICT,
-            detail = "Faculty already exists."
-        )
-
-    faculty_detail = Faculty(
-        user_id = faculty.user_id,
-        first_name = faculty.first_name,
-        last_name = faculty.last_name,
-        dob = faculty.dob,
-        sex = faculty.sex,
-        address = faculty.address,
-        date_of_joining = faculty.date_of_joining,
-        dept_code = faculty.dept_code,
-        role_id = faculty.role_id,
-    )
-
-    db.add(faculty_detail)
-    await db.commit()
-    await db.refresh(faculty_detail)
-
-    return faculty_detail
-
 # ------ GET FACULTY INFORMATION ROUTE BY QUERY PARAMETER ------
 @router.get("/search", response_model = list[FacultyResponse])
 async def get_faculty_info(
@@ -73,8 +28,10 @@ async def get_faculty_info(
     access: Annotated[User, Depends(admin_access)],
     db: Annotated[AsyncSession, Depends(get_db)]
 ):
+    # Query
     query = select(Faculty)
 
+    # Query parameter for each field
     if param.user_id is not None:
         query = query.where(Faculty.user_id.ilike(param.user_id))
     if param.first_name is not None:
@@ -105,17 +62,77 @@ async def get_faculty_info(
 
     return faculties
 
+# --------- CREATING FACULTY ROUTE ---------
+@router.post("/create", response_model = FacultyResponse)
+async def create_faculty(
+    faculty: FacultyCreate,
+    access: Annotated[User, Depends(admin_access)],
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+    # Check if user exists
+    user_exists = await db.scalar(
+        select(User).where(User.id == faculty.user_id)
+    )
+
+    # If not, raise HTTP exception
+    if user_exists is None:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "User not found."
+        )
+
+    # Check if the faculty exists
+    faculty_exists = await db.scalar(
+        select(Faculty).where(Faculty.user_id == faculty.user_id)
+    )
+
+    # If yes, raise HTTP exception
+    if faculty_exists is not None:
+        raise HTTPException(
+            status_code = status.HTTP_409_CONFLICT,
+            detail = "Faculty already exists."
+        )
+
+    # Object
+    faculty_detail = Faculty(
+        user_id = faculty.user_id,
+        first_name = faculty.first_name,
+        last_name = faculty.last_name,
+        dob = faculty.dob,
+        sex = faculty.sex,
+        address = faculty.address,
+        date_of_joining = faculty.date_of_joining,
+        dept_code = faculty.dept_code,
+        role_id = faculty.role_id,
+    )
+
+    db.add(faculty_detail)
+    await db.commit()
+    await db.refresh(faculty_detail)
+
+    return faculty_detail
+
+# ------- UPDATE FACULTY INFORMATION ROUTE -------
 @router.patch("/update/{emp_id}", response_model = FacultyResponse)
-async def partial_update(
+async def partial_update_faculty(
     emp_id: int,
     faculty: FacultyUpdate,
     access: Annotated[AsyncSession, Depends(admin_access)],
     db: Annotated[AsyncSession, Depends(get_db)]
 ):
+    # Check if the faculty exists
     updated_faculty = await db.scalar(
         select(Faculty).where(Faculty.emp_id == emp_id)
     )
 
+    # If not, raise HTTP exception
+    if updated_faculty is None:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Faculty doesn't exists."
+        )
+
+    # Setting the values into the new object
     for key, value in faculty.model_dump(exclude_unset = True).items:
         setattr(updated_faculty, key, value)
 
@@ -124,4 +141,26 @@ async def partial_update(
     await db.refresh(updated_faculty)
 
     return updated_faculty
+
+# --------- DELETE A FACULTY ROUTE ---------
+@router.delete("/delete/{emp_id}", status_code = status.HTTP_204_NO_CONTENT)
+async def delete_faculty(
+    emp_id: int,
+    access: Annotated[User, Depends(admin_access)],
+    db: Annotated[AsyncSession, Depends(get_db)]
+):
+    # Check if faculty exists
+    faculty = await db.scalar(
+        select(Faculty).where(Faculty.emp_id == emp_id)
+    )
+
+    # If not, raise HTTP exception
+    if faculty is None:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "Faculty doesn't exists."
+        )
+
+    await db.delete(faculty)
+    await db.commit()
 
