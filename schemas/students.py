050376d4067362848e408e9c9cb124c227b79390
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-13 10:43:02 +0530

    Add roll_no, start_batch_year, and end_batch_year fields to StudentCreate and StudentResponse schemas

--- schemas/students.py
+++ schemas/students.py
@@ -7,6 +7,7 @@ from models.faculty import SexEnum
 
 # Schema for creating a student
 class StudentCreate(BaseModel):
+    roll_no: int
     user_id: int
     first_name: str
     last_name: str
@@ -15,11 +16,14 @@ class StudentCreate(BaseModel):
     address: str
     date_of_admission: date
     dept_code: str
+    start_batch_year: int
+    end_batch_year: int
     role_id: int
 
 # Schema for response model of a student
 class StudentResponse(BaseModel):
     id: int
+    roll_no: int
     user_id: int
     first_name: str
     last_name: str
@@ -28,6 +32,8 @@ class StudentResponse(BaseModel):
     address: str
     date_of_admission: date
     dept_code: str
+    start_batch_year: int
+    end_batch_year: int
     role_id: int
     created_at: datetime
     updated_at: datetime
