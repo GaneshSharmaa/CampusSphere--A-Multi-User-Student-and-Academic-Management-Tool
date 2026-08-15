b46ae9b8d1dbe6c5a8837b19166d5d53ed0e55c9
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-08-15 11:37:26 +0530

    Add StudentUpdate schema for updating student records

--- schemas/students.py
+++ schemas/students.py
@@ -49,3 +49,15 @@ class StudentQueryParams(BaseModel):
     start_batch_year: int | None = None
     end_batch_year: int | None = None
 
+# Schema for updating student
+class StudentUpdate(BaseModel):
+    first_name: str | None = None
+    last_name: str | None = None
+    dob: date | None = None
+    sex: SexEnum | None = None
+    address: str | None = None
+    date_of_admission: date | None = None
+    start_batch_year: int | None = None
+    end_batch_year: int | None = None
+    dept_code: str | None = None
+
