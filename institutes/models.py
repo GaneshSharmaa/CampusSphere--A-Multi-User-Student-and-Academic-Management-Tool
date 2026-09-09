66e58b06aa7adfb55a3a323b7d978fec9f7c1a71
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-09-09 23:07:51 +0530

    Add Institute model with attributes and relationships

---
+++ institutes/models.py
@@ -0,0 +1,43 @@
+# Importing the local modules
+from database.database import Base
+
+# Importing the required modules
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import ForeignKey, String, Integer, Text, Date, DateTime, func
+from datetime import datetime
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from departments.models import Department
+
+class Institute(Base):
+    __tablename__ = "institutes"
+
+    id: Mapped[int] = mapped_column(
+        primary_key = True,
+        index = True
+    )
+    institute_code: Mapped[str] = mapped_column(
+        String(6),
+        index = True,
+        nullable = False,
+        unique = True
+    )
+    institute_name: Mapped[str] = mapped_column(
+        Text,
+        nullable = False
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_default = func.timezone("Asia/Kolkata", func.now()),
+        nullable = False
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone = True),
+        server_onupdate = func.timezone("Asia/Kolkata", func.now()),
+        nullable = False
+    )
+
+    # relationships
+    departments: Mapped["Department"] = relationship()
+    
