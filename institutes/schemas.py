c9c068667256cc23f6a98c6d85b65b403e9b4087
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-09-09 23:08:00 +0530

    Add schemas for Institute creation and response

---
+++ institutes/schemas.py
@@ -0,0 +1,19 @@
+# Importing the required modules
+from pydantic import BaseModel, Field, ConfigDict
+from datetime import datetime
+
+# Schema for creating an institution
+class InstituteCreate(BaseModel):
+    id: int
+    institute_name: str
+
+# Schema for having institution response
+class InstituteResponse(BaseModel):
+    id: int
+    institute_code: str
+    institute_name: str
+    created_at: datetime
+    updated_at: datetime
+
+    model_config = ConfigDict(from_attributes = True)
+
