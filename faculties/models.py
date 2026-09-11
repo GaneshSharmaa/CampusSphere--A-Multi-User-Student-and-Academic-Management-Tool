d02c954a262fcfd8c196dc9893e809b2096141a0
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-09-11 20:03:23 +0530

    Feat: Enhance Faculty model with institute relationship and additional fields for improved data structure

--- faculties/models.py
+++ faculties/models.py
@@ -13,6 +13,7 @@ if TYPE_CHECKING:
     from roles.models import Role
     from departments.models import Department
     from users.models import User
+    from institutes.models import Institute
 
 class SexEnum(Enum):
     MALE = "Male"
@@ -23,57 +24,71 @@ class Faculty(Base):
     __tablename__ = "faculty"
 
     emp_id: Mapped[int] = mapped_column(
-        Identity(start = 2),
         primary_key = True,
         index = True
     )
+
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id"),
         unique = True,
         nullable = False,
         index = True 
     )
+
     first_name: Mapped[str] = mapped_column(
         String(25),
         nullable = False
     )
+    
     last_name: Mapped[str] = mapped_column(
         String(25),
         nullable = False
     )
+
     dob: Mapped[date] = mapped_column(
         Date,
         nullable = False
     )
+
     sex: Mapped[SexEnum] = mapped_column(
         SQLEnum(SexEnum),
         nullable = False
     )
+
     address: Mapped[str] = mapped_column(
         Text,
         nullable = False
     )
+
     date_of_joining: Mapped[date] = mapped_column(
         Date,
         nullable = False
     )
+
     dept_id: Mapped[int] = mapped_column(
         ForeignKey("departments.id"),
-        nullable = False
+        nullable = False,
+        index = True
     )
+
     role_id: Mapped[int] = mapped_column(
         ForeignKey("roles.id"),
-        nullable = False
+        nullable = False,
+        index = True
     )
+
     institute_id: Mapped[int] = mapped_column(
         ForeignKey("institutes.id"),
-        nullable = False
+        nullable = False,
+        index = True
     )
+
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone = True),
         server_default = func.timezone("Asia/Kolkata", func.now()),
         nullable = False
     )
+
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone = True),
         server_default = func.timezone("Asia/Kolkata", func.now()),
@@ -93,6 +108,11 @@ class Faculty(Base):
 
     # relationship to department database model
     department: Mapped["Department"] = relationship(
+        back_populates = "faculties"
+    )
+
+    # relationship to institute database model
+    institute: Mapped["Institute"] = relationship(
         back_populates = "faculties"
     )
 
