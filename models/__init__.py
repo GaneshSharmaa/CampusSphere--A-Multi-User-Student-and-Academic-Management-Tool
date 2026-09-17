356dcbca77228a3c74e2cb57f187ba95524a2e5e
Ganesh Sharma <ganesh27sharma09@gmail.com>
2026-09-17 19:18:56 +0530

    Feat: Create __init__.py to import all SQLAlchemy models for the project

---
+++ models/__init__.py
@@ -0,0 +1,16 @@
+# Import all SQLAlchemy models
+from departments.models import Department
+from faculties.models import Faculty
+from roles.models import Role
+from students.models import Student
+from users.models import User
+from institutes.models import Institute
+from membership.models import Membership
+from programs.models import Program, ProgramSubject
+from subjects.models import Subject
+from attendance.models import Attendance
+from fees.models import Fee
+from marks.models import Mark
+from salary.models import Salary
+from schedules.models import Schedule
+
